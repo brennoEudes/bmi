da1aa85d4067a5d0e9b64b857fba458d873896b4
“Brenno <brennoeudes@gmail.com>
2024-01-31 17:08:59 -0300

    modal close structure

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,26 @@
 // OBJECT LITERAL: objeto responsável por orquestrar as funcionalidades.
 // É uma estrutura de dados c/ responsabilidades divididas.
 export const Modal = {
-    wrapper: document.querySelector(".modal-wrapper"),
-    message: document.querySelector(".modal .title span"),
-    btnClose: document.querySelector(".modal button.close"),
-  
-    // Shorthand de fcn disponível a partir do JS ES6:
-    // O formato abaixo é a msm coisa que open: function () {...alguma coisa}
-    open() {
-      Modal.wrapper.classList.add("open");
-    },
-    close() {
-      Modal.wrapper.classList.remove("open");
-    },
-  };
+  wrapper: document.querySelector(".modal-wrapper"),
+  message: document.querySelector(".modal .title span"),
+  btnClose: document.querySelector(".modal button.close"),
 
-  Modal.btnClose.onclick = () => Modal.close(); // Arrow fcn c/ 1 linha, podemos tirar as chaves!
+  // Shorthand de fcn disponível a partir do JS ES6:
+  // O formato abaixo é a msm coisa que open: function () {...alguma coisa}
+  open() {
+    Modal.wrapper.classList.add("open");
+  },
+  close() {
+    Modal.wrapper.classList.remove("open");
+  },
+};
+
+Modal.btnClose.onclick = () => Modal.close(); // Arrow fcn c/ 1 linha, podemos tirar as chaves!
+
+window.addEventListener("keydown", handleKeyDown);
+function handleKeyDown(event) {
+  if (event.key === "Escape") {
+    //console.log("Cliquei no esc!");
+    Modal.close();
+  }
+}
