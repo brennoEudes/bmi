db618c652b1fb74eea564630560400d95c581fe3
“Brenno <brennoeudes@gmail.com>
2024-01-31 11:10:08 -0300

    modal-wrapper result

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
 
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modalMessage = document.querySelector(".modal .title span");
+const modalBtnClose = document.querySelector(".modal button.close");
+
+
 // 3 maneiras de criar e atribuir função a um evento:
 // OBS: O método ON acessa diversas funcionalidades na DOM:
 
@@ -15,7 +20,14 @@ form.onsubmit = function (event) {
   const weight = inputWeight.value;
   const height = inputHeight.value;
 
-  console.log(weight, height);
+  // console.log(weight, height);
+
+  const result = BMI(weight, height);
+  const message = `Your BMI is ${result}.`
+  //console.log(result);
+
+  modalWrapper.classList.add("open");
+  modalMessage.innerHTML = message;
 };
 
 // 2º Arrow function:
@@ -24,3 +36,8 @@ form.onsubmit = function (event) {
 // 3º Criando uma fcn e passar o nome como referência:
 //form.onsubmit = handleSubmit
 //function handleSubmit() {}
+
+// fcn faz o cálculo do IMC:
+function BMI(weight, height) {
+  return (weight / height ** 2).toFixed(2);
+}
